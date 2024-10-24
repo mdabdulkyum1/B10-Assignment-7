70fe924b87ec6586069c9c2e6f641e6edb049ffb
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-24 22:18:59 +0600

    Ready to work and components setup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-600">
-      Welcome to Bpl
-    </h1>
+    <>
+
+    {/* Header here */}
+    {/* Main here */}
+    {/* Footer here */}
+
+    </>
   )
 }
 
