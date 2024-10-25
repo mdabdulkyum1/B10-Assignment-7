97a1e54eaa9f7668bcc55c0d350879c3df77a2fd
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-25 12:18:14 +0600

    footer design done and newsletter updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,50 @@
-
+import FooterLogo from "../../assets/logo-footer.png";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="bg-[#06091a] text-white  flex flex-col justify-center items-center">
+      <div className="mt-52">
+        <img src={FooterLogo} alt="BpL" />
+      </div>
+      <div className="container mx-auto mt-16 px-5 lg:px-0 grid grid-cols-1  lg:grid-cols-3">
+        <div className="">
+          <h1 className="font-bold text-xl mb-4">About Us</h1>
+          <p className="w-2/3 text-gray-500">
+            We are a passionate team dedicated to providing the best services to
+            our customers.
+          </p>
+        </div>
+        <div className="">
+          <h1 className="font-bold text-xl mb-4">Quick Links</h1>
+          <ul className="list-disc pl-6  text-gray-500">
+            <li>Home</li>
+            <li>Services</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="">
+          <h1 className="font-bold text-xl mb-4">Subscribe</h1>
+          <p className="text-gray-500">
+            Subscribe to our newsletter for the <br /> latest updates.
+          </p>
+          <div className="mt-6 flex flex-col lg:flex-row gap-4">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="input input-bordered text-gray-500"
+            />
+            <button className="btn bg-gradient-to-r from-pink-500 via-purple-400 to-yellow-400">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="border-t border-gray-700 w-full mt-11 py-8">
+        <p className="text-center text-gray-500">@2024 Your Company All Rights Reserved.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -2,16 +2,23 @@
 
 function Newsletter() {
   return (
-    <div className="w-3/4 mx-auto">
-      <div className="flex justify-center items-center p-6 border rounded-2xl">
+    <div className="w-3/4 mx-auto translate-y-[180px] ">
+      <div
+        className="flex justify-center items-center p-6 border border-white rounded-2xl bg-[#ffffff38]"
+        
+      >
         <div
           className="w-full rounded-2xl text-center py-20 bg-[linear-gradient(54deg,rgba(175,231,246,1)_0%,rgba(255,255,255,1)_18%,rgba(255,255,255,1)_82%,rgba(250,218,153,1)_100%)]"
-        //   style={{
-        //     backgroundImage: `url(${BgShadow})`,
-        //   }}
+          //   style={{
+          //     backgroundImage: `url(${BgShadow})`,
+          //   }}
         >
-          <h1 className="font-bold text-lg lg:text-3xl">Subscribe to our Newsletter</h1>
-          <p className="mt-4">Get the latest updates and news right in your inbox!</p>
+          <h1 className="font-bold text-lg lg:text-3xl">
+            Subscribe to our Newsletter
+          </h1>
+          <p className="mt-4">
+            Get the latest updates and news right in your inbox!
+          </p>
           <div className="mt-6 flex flex-col lg:flex-row justify-center gap-4">
             <input
               type="text"
