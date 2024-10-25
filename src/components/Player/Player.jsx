c6695d1a6c6edd50c8d89642b4d81a4b5a94e517
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-25 20:12:45 +0600

    display all players informations and players json data updated

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,12 +1,45 @@
+import PropTypes from "prop-types";
+import { FaUser } from "react-icons/fa";
+import { CiFlag1 } from "react-icons/ci";
 
-
-function Player() {
+function Player({ player }) {
+  const { name, image, country, role, price, rating} = player;
   return (
-    
-    <>
-      <h1>Players</h1>
-    </>
-  )
+    <div className="border rounded-2xl p-6">
+      <div className="w-12/12">
+        <img
+          src={image}
+          alt={`Player Name ${name}`}
+          className="w-full h-[250px] rounded-2xl"
+        />
+      </div>
+      <div className="flex items-center gap-4 mt-4">
+        <FaUser className="text-xl text-[#424242]"/>
+        <h1 className="font-bold text-xl">{name}</h1>
+      </div>
+      <div className="flex justify-between mt-6">
+        <div className="flex items-center gap-3">
+        <CiFlag1 />
+        <p>{country}</p>
+        </div>
+        <span>{role}</span>
+      </div>
+      <div className="divider"></div>
+      <p className="font-bold">Rating</p>
+      <div className="flex justify-between my-4">
+          <span className="font-bold">{rating?.batting_style}</span>
+          <span>{rating.bowling_style || "N/A"}</span>
+      </div>
+      <div className="flex justify-between">
+          <span className="font-bold">Price: ${price}</span>
+          <button className="btn py-2 px-4 border rounded">Choose Player</button>
+      </div>
+    </div>
+  );
 }
 
-export default Player
+Player.propTypes = {
+  player: PropTypes.object.isRequired,
+};
+
+export default Player;
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,62 +1,84 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Player from "./../Player/Player";
 import PlayerSelected from "./../PlayerSelected/PlayerSelected";
 
 function Players() {
+  // set all players data
+  const [players, setPlayers] = useState([]);
+  const [isToggle, setIsToggle] = useState({
+    available: true,
+    status: "available",
+  });
 
-  const [isToggle, setIsToggle] = useState({available:true, status:"available"})
-//  console.log(isToggle)
+  // fetch data
+  useEffect(() => {
+    fetch("players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
+  //  Toggling Feature
   const handelToggle = (value) => {
-    if(value === "available"){
-      setIsToggle({[value]:true, status:value});
-    }else{
-      setIsToggle({[value]:false, status:value})
+    if (value === "available") {
+      setIsToggle({ [value]: true, status: value });
+    } else {
+      setIsToggle({ [value]: false, status: value });
     }
-  }
-
+  };
 
   return (
     <div className="my-14 container mx-auto">
       <div className="flex flex-col lg:flex-row justify-between items-center">
         <div className="">
-          {
-            isToggle.available ? <h1 className="font-extrabold text-2xl">Available Players</h1> :  <h1 className="font-extrabold text-2xl">Selected Player (0/6)</h1>
-          }
-          
-         
+          {isToggle.available ? (
+            <h1 className="font-extrabold text-2xl">Available Players</h1>
+          ) : (
+            <h1 className="font-extrabold text-2xl">Selected Player (0/6)</h1>
+          )}
         </div>
         <div className="">
           <div className="flex rounded-xl border overflow-hidden">
-            <button onClick={()=>handelToggle("available")} className={`py-3 px-7 ${isToggle.available ? 'bg-[#e7fe29]' : '' } font-bold`}>
+            <button
+              onClick={() => handelToggle("available")}
+              className={`py-3 px-7 ${
+                isToggle.available ? "bg-[#e7fe29]" : ""
+              } font-bold`}
+            >
               Available
             </button>
-            <button onClick={()=>handelToggle("selected")} className={`py-3 px-7 ${isToggle.available ? '' : 'bg-[#e7fe29]' } font-bold`}>Selected (0)</button>
+            <button
+              onClick={() => handelToggle("selected")}
+              className={`py-3 px-7 ${
+                isToggle.available ? "" : "bg-[#e7fe29]"
+              } font-bold`}
+            >
+              Selected (0)
+            </button>
           </div>
         </div>
       </div>
 
       {/* All Players here */}
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="mt-10 grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {/* Player */}
-        {
-          isToggle.name === "available" && <Player />
-        } 
+        {isToggle.available &&
+          players.map((p) => <Player key={p.id} player={p}></Player>)}
 
         {/* Selected Player */}
       </div>
       <div className="">
-        {
-          isToggle.name === "selected" && <PlayerSelected />
-        }
+        {isToggle.name === "selected" && <PlayerSelected />}
       </div>
       <div className="text-center lg:text-left">
-
-        {
-              isToggle.status === "selected" &&  <span className="p-2 border border-black inline-block rounded-xl">
-         <button onClick={()=>handelToggle("available")} className="btn bg-[#e7fe29]">Add More Player</button>
-        </span>
-        }
-       
+        {isToggle.status === "selected" && (
+          <span className="p-2 border border-black inline-block rounded-xl">
+            <button
+              onClick={() => handelToggle("available")}
+              className="btn bg-[#e7fe29]"
+            >
+              Add More Player
+            </button>
+          </span>
+        )}
       </div>
     </div>
   );
