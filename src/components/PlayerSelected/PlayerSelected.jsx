5a51e76b4b1a0b5e43a3e30de86f018a0be5839f
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-10-26 14:50:52 +0600

    update playerselectd sectiosn

--- src/components/PlayerSelected/PlayerSelected.jsx
+++ src/components/PlayerSelected/PlayerSelected.jsx
@@ -4,9 +4,9 @@ import { PropTypes } from "prop-types";
 function PlayerSelected({ player, handelRemovePlayer }) {
   const { name, image, role } = player;
   return (
-    <div className="flex items-center justify-between border p-6 rounded-2xl my-6">
+    <div className="flex items-center justify-between border p-6 rounded-2xl my-6 shadow-lg">
       <div className="flex items-center gap-4">
-        <img src={image} alt={name} className="rounded-full h-28 w-28" />
+        <img src={image} alt={name} className="rounded-2xl h-28 w-28" />
         <div className="">
           <h1>{name}</h1>
           <p>{role}</p>
